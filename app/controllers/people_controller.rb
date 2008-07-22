f6834015be495a00a66d9caed2110009ca7cc281
Romain Durritcague <r.durritcague@studiomelipone.eu>
2008-07-22 17:15:27 +0200

    Added the simple captcha functionnality

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,11 +1,11 @@
 class PeopleController < ApplicationController
-  
+
   skip_before_filter :require_activation, :only => :verify
   skip_before_filter :admin_warning, :only => [ :show, :update ]
   before_filter :login_required, :only => [ :show, :edit, :update ]
   before_filter :correct_user_required, :only => [ :edit, :update ]
   before_filter :setup
-  
+
   def index
     @people = Person.mostly_active(params[:page])
 
@@ -13,7 +13,7 @@ class PeopleController < ApplicationController
       format.html
     end
   end
-  
+
   def show
     @person = Person.find(params[:id], :include => :activities)
     unless @person.active? or current_person.admin?
@@ -28,7 +28,7 @@ class PeopleController < ApplicationController
       format.html
     end
   end
-  
+
   def new
     @body = "register single-col"
     @person = Person.new
@@ -43,20 +43,25 @@ class PeopleController < ApplicationController
     @person = Person.new(params[:person])
     respond_to do |format|
       @person.email_verified = false if global_prefs.email_verifications?
-      @person.save
-      if @person.errors.empty?
-        if global_prefs.email_verifications?
-          @person.email_verifications.create
-          flash[:notice] = %(Thanks for signing up! A verification email has 
-                             been sent to #{@person.email}.)
-          format.html { redirect_to(home_url) }
+      if simple_captcha_valid?
+        @person.save
+        if @person.errors.empty?
+          if global_prefs.email_verifications?
+            @person.email_verifications.create
+            flash[:notice] = %(Thanks for signing up! A verification email has 
+            been sent to #{@person.email}.)
+            format.html { redirect_to(home_url) }
+          else
+            self.current_person = @person
+            flash[:notice] = "Thanks for signing up!"
+            format.html { redirect_back_or_default(home_url) }
+          end
         else
-          self.current_person = @person
-          flash[:notice] = "Thanks for signing up!"
-          format.html { redirect_back_or_default(home_url) }
+          @body = "register single-col"
+          format.html { render :action => 'new' }
         end
       else
-        @body = "register single-col"
+        flash[:error] = 'Captcha not valid'
         format.html { render :action => 'new' }
       end
     end
@@ -118,27 +123,27 @@ class PeopleController < ApplicationController
       end
     end
   end
-  
+
   def common_contacts
     @person = Person.find(params[:id])
     @common_connections = @person.common_connections_with(current_person,
-                                                          params[:page])
+    params[:page])
     respond_to do |format|
       format.html
     end
   end
-  
+
   private
 
-    def setup
-      @body = "person"
-    end
-  
-    def correct_user_required
-      redirect_to home_url unless Person.find(params[:id]) == current_person
-    end
-    
-    def preview?
-      params["commit"] == "Preview"
-    end
+  def setup
+    @body = "person"
+  end
+
+  def correct_user_required
+    redirect_to home_url unless Person.find(params[:id]) == current_person
+  end
+
+  def preview?
+    params["commit"] == "Preview"
+  end
 end
