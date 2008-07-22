8053c18af2945314cf47b95d3777d580bd513a3e
Romain Durritcague <r.durritcague@studiomelipone.eu>
2008-07-22 13:08:02 +0200

    Removed the 028 useless migration

--- db/migrate/028_add_state_column_to_people.rb
+++
@@ -1,9 +0,0 @@
-class AddStateColumnToPeople < ActiveRecord::Migration
-  def self.up
-    add_column      :people, :state, :string, :default => nil
-  end
-
-  def self.down
-    remove_column     :people, :state
-  end
-end
