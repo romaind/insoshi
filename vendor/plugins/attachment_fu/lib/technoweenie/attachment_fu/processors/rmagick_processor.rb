97d3893e4b1e4bd5802c3f2ef2e2e76d13e5bc42
Invité <invite@Markops.local>
2008-07-28 19:24:45 +0200

    Profile page with avatar and some personal data

--- vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu/processors/rmagick_processor.rb
+++ vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu/processors/rmagick_processor.rb
@@ -37,16 +37,16 @@ module Technoweenie # :nodoc:
         end
       
         # Performs the actual resizing operation for a thumbnail
-        def resize_image(img, size)
-          size = size.first if size.is_a?(Array) && size.length == 1 && !size.first.is_a?(Fixnum)
-          if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))
-            size = [size, size] if size.is_a?(Fixnum)
-            img.thumbnail!(*size)
-          else
-            img.change_geometry(size.to_s) { |cols, rows, image| image.resize!(cols, rows) }
-          end
-          self.temp_path = write_to_temp_file(img.to_blob)
-        end
+        def resize_image(img, size)  
+          size = size.first if size.is_a?(Array) && size.length == 1 && !size.first.is_a?(Fixnum)  
+          if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))  
+            size = [size, size] if size.is_a?(Fixnum)  
+            img.crop_resized!(*size)  
+          else  
+            img.change_geometry(size.to_s) { |cols, rows, image| image.crop_resized!(cols, rows) }  
+          end  
+          self.temp_path = write_to_temp_file(img.to_blob)  
+        end  
       end
     end
   end
