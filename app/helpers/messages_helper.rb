62ecd228e34c33c2989de00ad2d604e661fa95ea
Invité <invite@Markops.local>
2008-08-22 16:37:01 +0200

    Inbox buttons, wall comments visible for visitors, new requests link to portfolio and user can not edit empty project

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,8 +1,11 @@
 module MessagesHelper
   
   def list_link_with_active(name, options = {}, html_options = {}, &block)
-    name += "off.gif"
-    name += "on.gif" if current_page?(options)
+    if current_page?(options)
+      name += "on.gif"
+    else
+     name += "off.gif"
+    end
     content_tag(:li, link_to(image_tag(name), options, html_options, &block))
   end
 end
